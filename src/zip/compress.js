828fd151ab8e9aaaebf9c98fcec1afb90a22070e
andy <mill9r@gmail.com>
2025-10-26 19:34:25 +0100

    feature: implement zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs';
+import streams from 'node:stream/promises';
+import zlib from 'node:zlib';
+
 const compress = async () => {
-  // Write your code here
+  const fromFile = './files/fileToCompress.txt';
+  const toFile = './files/archive.gz'
+  const from = fs.createReadStream(fromFile, 'utf-8');
+  const to = fs.createWriteStream(toFile);
+  const gzip = zlib.createGzip();
+  
+  await streams.pipeline(from,gzip,to)
+
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs';
+import streams from 'node:stream/promises';
+import zlib from 'node:zlib';
+
 const decompress = async () => {
-  // Write your code here
+  const toFile = './files/fileToCompress.txt';
+  const fromFile = './files/archive.gz'
+  const from = fs.createReadStream(fromFile);
+  const to = fs.createWriteStream(toFile, 'utf-8');
+  const gzip = zlib.createGunzip();
+  
+  await streams.pipeline(from,gzip,to)
 };
 
 await decompress();

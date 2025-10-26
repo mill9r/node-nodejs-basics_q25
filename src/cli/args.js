545ea4be02fa488c40b3c3273b18bfa459b09e6b
andy <mill9r@gmail.com>
2025-10-26 19:16:59 +0100

    feature: implement CLI tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-  // Write your code here
+   const args = process.argv.slice(2);
+   const composedArgs = args.reduce((acc, arg, i, arr) => {
+    if(arg.startsWith('--')) {
+      acc[arg] = null
+    } else {
+      acc[arr[i-1]] = arg
+    }
+     return acc;
+   }, {})
+   const out = Object.entries(composedArgs).map(([k,v]) => `${k} is ${v}`)
+   console.log(out.join(', '))
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,6 @@
 const parseEnv = () => {
-  // Write your code here
+  const rssEnv = Object.entries(process.env).filter(([k,]) => k.startsWith('RSS_')).map(([k,v]) => `${k}=${v}`).join('; ');
+  console.log(rssEnv);
 };
 
 parseEnv();

d992a40b9a710dfdcd2d188ffdbd6ca8c9e01159
andy <mill9r@gmail.com>
2025-10-26 18:24:23 +0100

    feature: implement streams task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import stream from 'node:stream';
+import fs from 'node:fs';
+
 const write = async () => {
-  // Write your code here
+   const file = './files/fileToWrite.txt';
+   const writable = fs.createWriteStream(file);
+   process.stdin.on("data", data => {
+      writable.write(data)
+   })
 };
 
 await write();
